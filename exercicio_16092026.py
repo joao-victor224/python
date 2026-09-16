a347009e72b3c18086b496400aff929f5f17b165
joao-victor224 <joao.vrs5@puccampinas.edu.br>
2026-09-16 11:15:05 -0300

    Add files via upload

---
+++ exercicio_16092026.py
@@ -0,0 +1,10 @@
+produto= input ("Digite o nome do produto:")
+preço_unitártio =float(input("Digite o preço unitário:")) 
+quantidade=int(input("Digite a quantidade:"))
+
+valor_total_compra= quantidade * preço_unitártio
+
+print ("Nome do produto:",produto)
+print ("Preço unitário: R$",preço_unitártio)
+print("Quantidade:",quantidade)
+print("Valor total da compra: R$",valor_total_compra)
