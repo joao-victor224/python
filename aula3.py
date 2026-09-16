30a490a3011ddccbf99d6ead2a4745bcfb53291a
joao-victor224 <joao.vrs5@puccampinas.edu.br>
2026-09-16 11:14:46 -0300

    Add files via upload

---
+++ aula3.py
@@ -0,0 +1,14 @@
+#Entradas
+salario_fixo=1700
+vendas=230
+porcentual_comissão=15
+
+#Processo
+comissão = vendas * percentual_comissão / 100
+remuneração= salario_fixo + comissão
+
+#Saída 
+remuneração_total
+valor_comissão 
+print("A comissão é de R$", comissão)
+print("O salário total é de R$", remuneração)
